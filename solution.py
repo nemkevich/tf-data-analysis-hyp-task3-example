ecbdb0283483562e3ebd21ccd3fb0413f84dfe4b
Maxim Nemkevich <nemkevichma@gmail.com>
2023-04-13 18:13:03 +0200

    Attempt 1

--- solution.py
+++ solution.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as st
 
+chat_id = 82953459 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array, y: np.array) -> bool: 
+    p_value = st.permutation_test((x, y), lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis), 
+                 vectorized=True, 
+                 n_resamples=500,
+                 alternative='greater').pvalue 
+    alpha = 0.07
+    return p_value < alpha
